7ec70363d503785681144dec362bb025cb6296ec
Sourav Prasad <souravpd007@gmail.com>
2019-03-15 23:41:33 +0530

    enhanced

---
+++ client/index.php
@@ -0,0 +1,181 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="The Client side for the PetroBYTES platform">
+    <meta name="author" content="BYTES">
+
+    <title>PetroBytes Client</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="./css/bootstrap-italia.min.css" rel="stylesheet">
+    <link rel="icon" href="./faviconsmall.png" type="image/x-icon" />
+    <link rel="shortcut icon" href="./faviconsmall.png" type="image/x-icon" />
+</head>
+
+<body>
+    <div class="it-header-wrapper">
+        <div class="it-header-slim-wrapper">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="it-header-slim-wrapper-content">
+                            <a class="d-none d-lg-block navbar-brand" href="#"><img src="faviconsmall.png">
+                                PetroBYTES</a>
+                            <div class="nav-mobile">
+                                <nav>
+                                    <a class="it-opener d-lg-none" data-toggle="collapse" href="#menu-principale"
+                                        role="button" aria-expanded="false" aria-controls="menu-principale">
+                                        <span>PetroBYTES</span>
+                                        <svg class="icon">
+                                            <use xlink:href="./svg/sprite.svg#it-expand"></use>
+                                        </svg>
+                                    </a>
+                                    <div class="link-list-wrapper collapse" id="menu-principale">
+                                        <ul class="link-list">
+                                            <li><a href="../administrator">Administrator</a></li>
+                                            <li><a href="#">Contact</a></li>
+                                        </ul>
+                                    </div>
+                                </nav>
+                            </div>
+                            <div class="header-slim-right-zone">
+                                <div class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown"
+                                        aria-expanded="false">
+                                        <span>Browse</span>
+                                        <svg class="icon d-none d-lg-block">
+                                            <use xlink:href="./svg/sprite.svg#it-expand"></use>
+                                        </svg>
+                                    </a>
+                                    <div class="dropdown-menu">
+                                        <div class="row">
+                                            <div class="col-12">
+                                                <div class="link-list-wrapper">
+                                                    <ul class="link-list">
+                                                        <li><a class="list-item" href="#"><span>A</span></a></li>
+                                                        <li><a class="list-item" href="#"><span>B</span></a></li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="it-access-top-wrapper">
+                                    <button class="btn btn-primary btn-sm" href="#" type="button">LOG IN</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="it-nav-wrapper">
+            <div class="it-header-center-wrapper">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="it-header-center-content-wrapper">
+                                <div class="it-brand-wrapper">
+                                    <a href="#">
+                                        <img src="favicon.png" alt="">
+                                        <div class="it-brand-text">
+                                            <h2 class="no_toc">PetroBYTES</h2>
+                                            <h3 class="no_toc d-none d-md-block">Client</h3>
+                                        </div>
+                                    </a>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="container my-4">
+      <h1>Enter OTP</h1>
+      <form action="">
+        <div class="form-group">
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <div class="input-group-text"><svg class="icon icon-sm icon-primary">
+                            <use xlink:href="./svg/sprite.svg#it-pencil"></use>
+                        </svg></div>
+                </div>
+                <label for="input-group-3">Please Enter your OTP</label>
+                <input type="text" class="form-control" id="input-group-3" name="input-group-3">
+                <div class="input-group-append">
+                    <button class="btn btn-primary" type="button" id="button-3">Submit</button>
+                </div>
+            </form>
+            </div>
+        </div>
+    </div>
+
+    <footer class="it-footer">
+        <div class="it-footer-main">
+            <div class="container">
+                <section>
+                    <div class="row clearfix">
+                        <div class="col-sm-12">
+                            <div class="it-brand-wrapper">
+                                <a href="#">
+                                    <svg class="icon">
+                                        <use xlink:href="./svg/sprite.svg#it-code-circle"></use>
+                                    </svg>
+                                    <div class="it-brand-text">
+                                        <h2 class="no_toc">PetroBYTES</h2>
+                                        <h3 class="no_toc d-none d-md-block">Client Side</h3>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section>
+
+                </section>
+                <section class="py-4 border-white border-top">
+                    <div class="row">
+                        <div class="col-lg-4 col-md-4 pb-2">
+                            <h4><a href="#" title="Vai alla pagina: Contatti">Contact</a></h4>
+                            <p>
+                                <strong>IIT ISM DHANBAD</strong><br> DHANBAD-826004 JHARKHAND
+                            </p>
+                            <div class="link-list-wrapper">
+                                <ul class="footer-list link-list clearfix">
+                                    <li><a class="list-item" href="#"
+                                            title="Vai alla pagina: Posta Elettronica Certificata">Github Code</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-4 pb-2">
+                            <h4><a href="#" title="Vai alla pagina: Lorem Ipsum">About</a></h4>
+                        </div>
+                        <div class="col-lg-4 col-md-4 pb-2">
+
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+        <div class="it-footer-small-prints clearfix">
+            <div class="container">
+                <h3 class="sr-only">Links</h3>
+                <ul class="it-footer-small-prints-list list-inline mb-0 d-flex flex-column flex-md-row">
+                    <li class="list-inline-item"><a href="#" title="Note Legali">Media policy</a></li>
+                    <li class="list-inline-item"><a href="#" title="Note Legali">Legal Notices</a></li>
+                    <li class="list-inline-item"><a href="#" title="Privacy-Cookies">Privacy policy</a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+
+    <script src="./js/bootstrap-italia.bundle.min.js"></script>
